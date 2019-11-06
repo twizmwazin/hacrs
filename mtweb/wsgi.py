e8d5765744bad62ae9577b08ca5038922df929dc
Tariq M Nasim <tnasim@asu.edu>
2019-11-06 05:42:39 -0700

    Updated the wsgi.py file

--- mtweb/wsgi.py
+++ mtweb/wsgi.py
@@ -1,7 +1,6 @@
 #from HaCRSUI import app
 #app.run(host="0.0.0.0", port=8182)
 
-'''
 import sys
 
 ADMIN = 1
@@ -29,4 +28,3 @@ if __name__ == "__main__":
 			main(USER)
 
     #app.run(port=8182)
-'''
